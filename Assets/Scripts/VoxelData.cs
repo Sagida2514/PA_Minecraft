51c92b65ed7c63de99397f1314c09c9a5e0cea85
Sagida2514 <90020964+Sagida2514@users.noreply.github.com>
2023-07-30 03:33:52 +0900

    dz

---
+++ Assets/Scripts/VoxelData.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoxelData : MonoBehaviour
+{
+    public static readonly Vector3[] voxelVerts = new Vector3[8]
+    {
+        new Vector3(0.0f, 0.0f, 0.0f),
+        new Vector3(1.0f, 0.0f, 0.0f),
+        new Vector3(0.0f, 1.0f, 0.0f),
+        new Vector3(0.0f, 1.0f, 0.0f),
+        new Vector3(0.0f, 0.0f, 1.0f),
+        new Vector3(1.0f, 0.0f, 1.0f),
+        new Vector3(1.0f, 1.0f, 1.0f),
+        new Vector3(0.0f, 1.0f, 1.0f),
+    };
+}
